73405f2cdfef3a06c9f2d688a65b6dc14d957281
Ntuthuko Ndlovu <ntuthuko.s.ndlovu@gmail.com>
2023-01-05 15:14:58 +0200

    added image, styling, fetching from API

--- script.js
+++ script.js
@@ -17,13 +17,32 @@ let getMovie = () => {
         fetch(url)
         .then((resp) => resp.json())
         .then((data) => {
-            console.log(data);
-            console.log(data.Poster);
-            console.log(data);
-            console.log(data);
-            console.log(data);
+          result.innerHTML = `
+            <div class="info">
+               <img src=${data.Poster} class="poster">
+            <div>
+               <h2>${data.Title}</h2>
+            <div class="rating">
+               <img src="star-icon.svg">
+               <h4>${data.imdbRating}</h4>
+            </div>
+            <div class="details">
+                <span>${data.Rated}</span>
+                <span>${data.Year}</span>
+                <span>${data.Runtime}</span>
+            </div>
+            <div class"genre">
+                <div>${data.Genre.split(",").join("<div></div>")}</div>
+            </div>
+            </div>
+            </div>
+            <h3>Plot:</h3> 
+                <p>${data.Plot}</p>  
+            <h3>Cast:</h3> 
+                <p>${data.Actors}</p> 
+            `;
         });
     }
 };
 
-window.addEventListener("load",getMovie);
+window.addEventListener("load", getMovie);
